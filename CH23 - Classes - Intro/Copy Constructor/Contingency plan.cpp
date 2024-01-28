88a41c7a8a6500455c78c3dd58e44a47c0c36102
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-28 17:24:11 +0800

    Committed on or around 2024/01/28

---
+++ CH23 - Classes - Intro/Copy Constructor/Contingency plan.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+
+private:
+	int x, y;
+
+public:
+
+	MyClass(int xx, int yy) :x{ xx }, y{ yy }{
+
+
+	}
+
+};
+
+
+
+int main() {
+
+
+
+}
