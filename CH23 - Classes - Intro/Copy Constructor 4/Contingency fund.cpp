04a7d8b9f1276fd52229f45fe5c0a6ef45ca2d45
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-29 23:48:29 +0800

    Committed on or around 2024/01/29

---
+++ CH23 - Classes - Intro/Copy Constructor 4/Contingency fund.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+
+private:
+	int  x;
+	int* p;
+
+public:
+
+	MyClass(int xx, int pp) :x{ xx }, p{ new int{pp} }
+	{
+		cout << "User-defined constructor invoked.\n";
+	}
+
+	MyClass(const MyClass& obj) :x{ obj.x }, p{ new int{*obj.p} }
+	{
+		cout << "Copy constructor invoked.\n";
+	}
+
+};
+
+
+
+int main() {
+
+	MyClass o2 = o1;
+
+	MyClass o1{ 1, 2 };
+
+}
